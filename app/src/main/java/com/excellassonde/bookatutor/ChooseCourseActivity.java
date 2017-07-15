66b0ad0a19cc27789cd91e6af07cc78c8ed79347
Praise Ayorinde <praiseayorinde@e75-u02-x2100.mgmt.yorku.ca>
2017-07-15 13:58:57 -0400

    first version

---
+++ app/src/main/java/com/excellassonde/bookatutor/ChooseCourseActivity.java
@@ -0,0 +1,32 @@
+package com.excellassonde.bookatutor;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.view.ViewPropertyAnimatorCompatSet;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+public class ChooseCourseActivity extends AppCompatActivity {
+
+    private Spinner spinner;
+    public static String courseSelected;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_course);
+
+    }
+
+    public void chooseCourse(View view){
+        spinner = (Spinner) findViewById(R.id.courses_dropdown);
+        Intent intent = new Intent(this, ChooseDayActivity.class);
+        String courseChoice = String.valueOf(spinner.getSelectedItem());
+        intent.putExtra(courseSelected, courseChoice);
+        startActivity(intent);
+
+    }
+}
